e2f80aa31f60fd9a0bfa073f46505f7d7e2f598e
NATHALIA VENTURA KAWAKAMI <23111822@CFP127ALUNO.LOCAL>
2024-03-07 17:07:50 -0300

    overscroll not working :D

--- js/main.js
+++ js/main.js
@@ -2,33 +2,38 @@
 
 import { getFilmes, getFilme } from "./filmes.js"
 
-// console.table(await getFilmes())
+console.table(await getFilmes())
 // console.table(await getFilme(1))
 
 function criarCard(filme){
     const card = document.createElement('div')
-    const titulo = document.createElement('h2')
-    titulo.textContent = filme.nome 
-    const texto = document.createElement('textarea')
-    texto.textContent = filme.sinopse
+    // const titulo = document.createElement('h2')
+    // titulo.textContent = filme.nome 
+    // const texto = document.createElement('textarea')
+    // texto.textContent = filme.sinopse
+    const foto = document.createElement('img')
+    foto.src = filme.foto_capa
+    foto.classList.add('w-44', 'h-60')
 
-    card.append(titulo, texto)
+    card.append(foto)
 
     return card
 }
 
 async function preencherContainer(){
-    const container = document.querySelector('body')
+    const container = document.getElementById('filmes')
 
     const filmes = await getFilmes()
 
-    filmes.forEach(filme => {
-        const card = criarCard(filme)
-        container.appendChild(card)
-        
-    })
+    for (let index = 0; index < 4; index++) {
+        filmes.forEach(filme => {
+            const card = criarCard(filme)
+            container.appendChild(card)
+            console.log(card)
+        })
+    }
 }
 
 
 
-//preencherContainer()
+preencherContainer()
